48b2b539a93a2f299516df6c839425b96e5e1d45
RED9OG <noobcoder56@gmail.com>
2024-02-18 23:20:38 +0530

    first commit

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "JetBrainsMono Nerd Font  :pixelsize=18:antialias=true:autohint=true:style=SemiBold";
-static char *font2[] = {"JetBrainsMono Nerd Font  :pixelsize=18:antialias=true:autohint=true:style=SemiBold"};
+static char *font2[] = { "Iosevka Nerd Font :pixelsize=18:antialias=true:autohint=true" };
 static int borderpx = 0;
 
 /*
